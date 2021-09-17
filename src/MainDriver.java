98c95a5d5fe283c5b902babc41a936b8902853b0
Raul Rosales <raurosales@paypal.com>
2021-09-16 21:29:28 -0700

    corrected exeption error and updated main menu

--- src/MainDriver.java
+++ src/MainDriver.java
@@ -10,7 +10,29 @@ public class MainDriver {
 
     public static void main(String[] args) throws IOException {
         BankDataInfo.readFile("C:\\Users\\raurosales\\Desktop\\Project1\\java-project-1\\src\\BankData.txt");
+        Scanner input = new Scanner(System.in);
 
+        int mainMenuChoice;
+
+        {
+            System.out.println("Welcome to RosalesInc. Bank ");
+            System.out.println("----------------------------");
+            System.out.println ( "Please Make a Selection") ;
+            System.out.println ( "1. Login to Existing Account" );
+            System.out.println ( "2. Create a New Account" ) ;
+            System.out.println ( "\n\nEnter a number from 1 to 2" );
+            mainMenuChoice = input.nextInt();
+            if (mainMenuChoice == 1) {
+                UserFunctionality.userLogin(10001, "juagw362");
+            }
+                    else if (mainMenuChoice == 2) {
+                    UserFunctionality.newAccountCreation();
+            }else
+            System.out.println("Invalid menu option ");
+        }
+
+//
+//        System.out.println("Hello World");
 //        UserFunctionality.newAccountCreation();
 
     }
